c51cf1ea674fc2d92a1f37e70bc90d02fce16de5
suryansh gupta <suryanshgupta93@gmail.com>
2022-10-14 17:59:18 +0530

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,49 +1,59 @@
 // import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [amount, setamount] = useState("100");
 
-  const [amount, setamount] = useState('100');
-
-  const handleSubmit = (e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    
-      var options = {
-        key: "rzp_test_f89aKkLfxrvOju",
-        key_secret:"8pU4i3wKIqKO6W6O5b0y7PTf",
-        amount: amount *100,
-        currency:"INR",
-        name:"air-bnb",
-        description:"for testing purpose",
-        handler: function(response){
-          alert(response.razorpay_payment_id);
-        },
-        prefill: {
-          name:"Velmurugan",
-          email:"mvel1620r@gmail.com",
-          contact:"9958037327"
-        },
-        notes:{
-          address:"Razorpay Corporate office"
-        },
-        theme: {
-          color:"#3399cc"
-        }
-      };
-      var pay = new window.Razorpay(options);
-      pay.open();
-    
-  }
-  
+
+    var options = {
+      key: "rzp_test_f89aKkLfxrvOju",
+      key_secret: "8pU4i3wKIqKO6W6O5b0y7PTf",
+      amount: amount * 100,
+      currency: "INR",
+      name: "air-bnb",
+      description: "for testing purpose",
+      handler: function (response) {
+        alert(response.razorpay_payment_id);
+      },
+      prefill: {
+        name: "suryansh",
+        email: "suryansh@gmail.com",
+        contact: "9958037327",
+      },
+      notes: {
+        address: "Razorpay Corporate office",
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
+    var pay = new window.Razorpay(options);
+    pay.open();
+  };
+
   return (
     <div className="App">
-    <h2>Razorpay Payment Integration Using React</h2>
-    <br/>
-    <input type="text"placeholder='Enter Amount'value={amount}onChange={(e)=>setamount(e.target.value)} />
-    <br/><br/>
-    <button value={amount}onChange={(e)=>setamount(e.target.value)} onClick={handleSubmit}>submit</button>
-   </div>
+      <h2>Razorpay Payment Integration Using React</h2>
+      <br />
+      <input
+        type="text"
+        placeholder="Enter Amount"
+        value={amount}
+        onChange={(e) => setamount(e.target.value)}
+      />
+      <br />
+      <br />
+      <button
+        value={amount}
+        onChange={(e) => setamount(e.target.value)}
+        onClick={handleSubmit}
+      >
+        submit
+      </button>
+    </div>
   );
 }
 
